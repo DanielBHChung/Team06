dc32aff6eecc3501bfbcebfed80fa78ed52906a1
Daniel Chung <danielchung93@gmail.com>
2020-11-18 10:14:22 -0800

    changes to main.html and app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
-function readForum(){
+function readForum() {
     db.collection("forum").doc("01")
-    .onSnapshot(function(snap){
-        console.log(snap.data());   //print the document fields of "01"
-        console.log(snap.data().post);     //spelled EXACTLY as the firestore
-        document.getElementById("abc").innerText = snap.data().post;
-    })
+        .onSnapshot(function (snap) {
+            console.log(snap.data()); //print the document fields of "01"
+            console.log(snap.data().post); //spelled EXACTLY as the firestore
+            document.getElementById("abc").innerText = snap.data().post;
+        })
 }
 readForum();
