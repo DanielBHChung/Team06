124412c3ca5205076761497d2eb23070e3ed644c
teplatformdesign <teplatformdesign@gmail.com>
2020-11-18 10:10:37 -0800

    revamp main and index

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function toggleComment() {
+    var x = document.getElementById("commentarea");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
