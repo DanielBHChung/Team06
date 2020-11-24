b8ef05528205bd4f70ce4f47b74480ec4d5bf976
teplatformdesign <teplatformdesign@gmail.com>
2020-11-23 18:53:45 -0800

    GOT DA COMMENT WORKING YO

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-function readForum() {
-    db.collection("forum").doc("01")
-        .onSnapshot(function (snap) {
-            console.log(snap.data()); //print the document fields of "01"
-            console.log(snap.data().post); //spelled EXACTLY as the firestore
-            document.getElementById("abc").innerText = snap.data().post;
-        })
-}
-readForum();
